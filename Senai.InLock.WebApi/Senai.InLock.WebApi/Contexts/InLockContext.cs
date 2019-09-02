15ce61e1aa9aeb6072ba7f3837d3f23739d937cc
Gabriel Schorsch Amadeu <gabrielschorsch@outlook.com>
2019-09-02 14:55:15 -0300

    Primeiro Commit

---
+++ Senai.InLock.WebApi/Senai.InLock.WebApi/Contexts/InLockContext.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Senai.InLock.WebApi.Domains
+{
+    public partial class InLockContext : DbContext
+    {
+        public InLockContext()
+        {
+        }
+
+        public InLockContext(DbContextOptions<InLockContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Estudios> Estudios { get; set; }
+        public virtual DbSet<Jogos> Jogos { get; set; }
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Data Source = .\\SqlExpress;Initial Catalog = T_InLock;User Id=sa;Pwd=132;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Estudios>(entity =>
+            {
+                entity.HasKey(e => e.EstudioId);
+
+                entity.HasIndex(e => e.NomeEstudio)
+                    .HasName("UQ__Estudios__112A56909D3B85B1")
+                    .IsUnique();
+
+                entity.Property(e => e.DataCriacao).HasColumnType("smalldatetime");
+
+                entity.Property(e => e.NomeEstudio)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.PaisOrigem)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.HasOne(d => d.Usuario)
+                    .WithMany(p => p.Estudios)
+                    .HasForeignKey(d => d.UsuarioId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Estudios__Usuari__52593CB8");
+            });
+
+            modelBuilder.Entity<Jogos>(entity =>
+            {
+                entity.HasKey(e => e.JogoId);
+
+                entity.HasIndex(e => e.NomeJogo)
+                    .HasName("UQ__Jogos__89AF93E40E476AA0")
+                    .IsUnique();
+
+                entity.Property(e => e.DataLancamento).HasColumnType("date");
+
+                entity.Property(e => e.Descricao)
+                    .HasMaxLength(999)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NomeJogo)
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Valor).HasColumnType("smallmoney");
+
+                entity.HasOne(d => d.Estudio)
+                    .WithMany(p => p.Jogos)
+                    .HasForeignKey(d => d.EstudioId)
+                    .OnDelete(DeleteBehavior.ClientSetNull)
+                    .HasConstraintName("FK__Jogos__EstudioId__59FA5E80");
+            });
+
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.HasKey(e => e.UsuarioId);
+
+                entity.HasIndex(e => e.Email)
+                    .HasName("UQ__Usuarios__A9D1053470CFE891")
+                    .IsUnique();
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Permissao)
+                    .HasMaxLength(255)
+                    .IsUnicode(false)
+                    .HasDefaultValueSql("('CLIENTE')");
+
+                entity.Property(e => e.Senha)
+                    .IsRequired()
+                    .HasMaxLength(255)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
